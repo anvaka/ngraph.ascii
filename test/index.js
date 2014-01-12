8bce02d481c092e75f4141b47b7669c501b8deac
anvaka <anvaka@gmail.com>
2014-01-12 13:06:33 -0800

    Fixed array screen for large values

--- test/index.js
+++ test/index.js
@@ -24,3 +24,21 @@ test('it renders graph', function (t) {
     t.equals(starsCount, 2, 'Found two stars for each node');
   }
 });
+
+
+test('screen chooses correct coordinates', function (t) {
+  var screen = arrayScreen(80, 24, verifyFrame);
+
+  screen.put(0, 0, '0'); // 0
+  screen.put(1, 0, '1'); // 1
+  screen.put(0, 1, '2'); // 80
+  screen.put(82, 1, '3'); // 159
+  screen.flush();
+  function verifyFrame(chars) {
+    t.equals(chars[0], '0');
+    t.equals(chars[1], '1');
+    t.equals(chars[80], '2');
+    t.equals(chars[159], '3');
+    t.end();
+  }
+});
