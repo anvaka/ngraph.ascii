cb47656ea6e490c7d49f914d2fe1060f912a13e1
anvaka <anvaka@gmail.com>
2014-03-04 01:22:43 -0800

    Added npm visualization. Cause why not

---
+++ demo/npmgraph.js
@@ -0,0 +1,34 @@
+var q = require('q');
+var http = require('http');
+var querystring = require('querystring');
+var graph = require('ngraph.graph')();
+var graphBuilder = require('npmgraphbuilder')(httpClient);
+
+graphBuilder.createNpmDependenciesGraph('browserify', graph)
+  .then(renderGraph)
+  .fail(function (err) {
+    console.error('Failed to build graph: ', err);
+  });
+
+function renderGraph(graph) {
+  var asciiGraphics = require('../').graphics(graph);
+
+  asciiGraphics.run();
+}
+
+
+function httpClient(url, data) {
+  console.log('getting ' + url);
+  var defer = q.defer();
+  http.get(url + '?' + querystring.stringify(data), function (res) {
+    var body = '';
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      body += chunk;
+    }).on('end', function () {
+      defer.resolve({ data: JSON.parse(body) });
+    });
+  });
+
+  return defer.promise;
+}
